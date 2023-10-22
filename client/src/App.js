2f5f21fa0711774d25d413922ca11cd2f768aa01
Brian Sikute <brian.sikute@gmail.com>
2023-10-22 23:24:59 +0200

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Docker
+            Learn Docker-K8!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
