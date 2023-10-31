c8d2bcfe7bf07873d8e977ef72fd6a1cae0115a1
Brian Sikute <brian.sikute@gmail.com>
2023-10-31 15:14:44 +0200

    deploying Budapest 8

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Docker and K88 now!!
+            Learn Docker and K88 from Budapest!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
